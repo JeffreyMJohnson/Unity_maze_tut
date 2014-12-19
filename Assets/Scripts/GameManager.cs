ca3fc6b04483069238e8157213b9c7ab48d65643
JeffreyMJohnson <jefjohms@gmail.com>
2014-12-18 18:46:06 -0800

    Initial commit.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+	// Use this for initialization
+	void Start () {
+        BeginGame();
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.Space))
+    {
+        RestartGame();
+    }
+	}
+
+    private void RestartGame()
+    {
+        Destroy(mazeInstance.gameObject);
+        BeginGame();
+    } 
+    
+    private void BeginGame()
+    {
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+    }
+}
