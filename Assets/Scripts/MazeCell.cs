5df6e2b44814e8cc633d6a3de1ab771810a00a62
JeffreyMJohnson <jefjohms@gmail.com>
2014-12-19 15:55:38 -0800

    added random placement, walls and passages.

--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -4,6 +4,16 @@ public class MazeCell : MonoBehaviour {
 
     public IntVector2 coordinates;
 
+    private MazeCellEdge[] edges = new MazeCellEdge[MazeDirections.count];
+
+    public MazeCellEdge GetEdge(MazeDirection direction)
+    {
+        return edges[(int)direction];
+    }
+    public void SetEdge(MazeDirection direction, MazeCellEdge edge)
+    {
+        edges[(int)direction] = edge;
+    }
 	// Use this for initialization
 	void Start () {
 	
@@ -13,4 +23,6 @@ public class MazeCell : MonoBehaviour {
 	void Update () {
 	
 	}
+
+
 }
---
+++ Assets/Scripts/MazeDirections.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public enum MazeDirection
+{
+    North,
+    East,
+    South,
+    West
+}
+
+public static class MazeDirections
+{
+    public const int count = 4;
+
+    public static MazeDirection RandomValue
+    {
+        get
+        {
+            return (MazeDirection)Random.Range(0, count);
+        }
+    }
+
+    public static IntVector2 ToIntVector2(this MazeDirection direction)
+    {
+        return vectors[(int)direction];
+    }
+
+    public static MazeDirection GetOpposite(this MazeDirection direction)
+    {
+        return opposites[(int)direction];
+    }
+
+    public static Quaternion ToRotation(this MazeDirection direction)
+    {
+        return rotations[(int)direction];
+    }
+
+    private static IntVector2[] vectors = {
+        new IntVector2(0,1),
+        new IntVector2(1,0),
+        new IntVector2(0,-1),
+        new IntVector2(-1,0)
+    };
+
+    private static MazeDirection[] opposites = 
+    {
+        MazeDirection.South,
+        MazeDirection.West,
+        MazeDirection.North,
+        MazeDirection.East
+    };
+
+    private static Quaternion[] rotations = 
+    {
+        Quaternion.identity,
+        Quaternion.Euler(0f,90f,0f),
+        Quaternion.Euler(0f, 180f, 0f),
+        Quaternion.Euler(0f, 270f, 0f)
+    };
+
+}
