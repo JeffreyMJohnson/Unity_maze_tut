48fca2e6bb1acd9750a35184e148263b96d4cc70
JeffreyMJohnson <jefjohms@gmail.com>
2014-12-20 13:16:05 -0800

    decorate maze

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -11,7 +11,7 @@ public class Maze : MonoBehaviour {
 	public float generationStepDelay;
 
 	public MazePassage passagePrefab;
-	public MazeWall wallPrefab;
+	public MazeWall[] wallPrefabs;
 
 	private MazeCell[,] cells;
 
@@ -87,10 +87,10 @@ public class Maze : MonoBehaviour {
 	}
 
 	private void CreateWall (MazeCell cell, MazeCell otherCell, MazeDirection direction) {
-		MazeWall wall = Instantiate(wallPrefab) as MazeWall;
+		MazeWall wall = Instantiate(wallPrefabs[Random.Range(0, wallPrefabs.Length)]) as MazeWall;
 		wall.Initialize(cell, otherCell, direction);
 		if (otherCell != null) {
-			wall = Instantiate(wallPrefab) as MazeWall;
+			wall = Instantiate(wallPrefabs[Random.Range(0, wallPrefabs.Length)]) as MazeWall;
 			wall.Initialize(otherCell, cell, direction.GetOpposite());
 		}
 	}
