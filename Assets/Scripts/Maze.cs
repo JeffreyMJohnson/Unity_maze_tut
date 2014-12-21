37dd99ce67336890f9bf7a9cdcb0477eda6a4be6
JeffreyMJohnson <jefjohms@gmail.com>
2014-12-21 14:56:01 -0800

    finish hiding rooms

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -48,6 +48,10 @@ public class Maze : MonoBehaviour {
 			yield return delay;
 			DoNextGenerationStep(activeCells);
 		}
+        for (int i = 0; i < rooms.Count; ++i)
+        {
+            rooms[i].Hide();
+        }
 	}
 
 	private void DoFirstGenerationStep (List<MazeCell> activeCells) {
--- Assets/Scripts/MazeCell.cs
+++ Assets/Scripts/MazeCell.cs
@@ -47,6 +47,7 @@ public class MazeCell : MonoBehaviour {
 
     public void OnPlayerEntered()
     {
+        room.Show();
         for (int i = 0; i < edges.Length; ++i)
         {
             edges[i].OnPlayerEntered();
@@ -55,9 +56,20 @@ public class MazeCell : MonoBehaviour {
 
     public void OnPlayerExited()
     {
+        room.Hide();
         for (int i = 0; i < edges.Length; ++i)
         {
             edges[i].OnPlayerExited();
         }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
     }
 }
